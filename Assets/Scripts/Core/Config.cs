50c0be4744326fcc456fc59086771bca714badd3
KD_Han <gkswjdgur552@gmail.com>
2022-09-12 23:14:28 +0900

    Fix Config

--- Assets/Scripts/Core/Config.cs
+++ Assets/Scripts/Core/Config.cs
@@ -3,57 +3,66 @@ public class Config : MonoBehaviourSingleton<Config>
     //------------------------- URL ---------------------------------
     //WakZoo
     public string WakZoo { get; internal set; }
+
+    //Origin URL
+    public string Origin_Rewind { get; internal set; }
+    public string Origin_WinterSpring { get; internal set; }
+
     //BackGroundMusic
     public string BGM_01 { get; internal set; }
     public string BGM_02 { get; internal set; }
     public string BGM_03 { get; internal set; }
 
-    //Music
-    public string Rewind { get; internal set; }
-    public string WinterSpring { get; internal set; }
+    //ReMix URL
+    //Music       
+    public string ReMix_Rewind { get; internal set; }
+    public string ReMix_WinterSpring { get; internal set; }
+                  
+    public string ReMix_NobleLick { get; internal set; }
+    public string ReMix_Wakaloid { get; internal set; }
+    public string ReMix_WakGoodAroma100 { get; internal set; }
+    public string ReMix_AvantGarde { get; internal set; }
 
-    public string NobleLick { get; internal set; }
-    public string Wakaloid { get; internal set; }
-    public string WakGoodAroma100 { get; internal set; }
-    public string AvantGarde { get; internal set; }
-
-    public string YouHi { get; internal set; }
-    public string Gotterfly { get; internal set; }
-    public string KingADance { get; internal set; }
-    public string IPad { get; internal set; }
-    public string LikeDogRevival { get; internal set; }
-
-    public string GalUseGirl { get; internal set; }
-    public string BangOff { get; internal set; }
-    public string AfterTwist { get; internal set; }
-    public string Waklio { get; internal set; }
+    public string ReMix_YouHi { get; internal set; }
+    public string ReMix_Gotterfly { get; internal set; }
+    public string ReMix_KingADance { get; internal set; }
+    public string ReMix_IPad { get; internal set; }
+    public string ReMix_ReviveLikeADog { get; internal set; }
+                  
+    public string ReMix_GalUseGirl { get; internal set; }
+    public string ReMix_BangOff { get; internal set; }
+    public string ReMix_TwistedLove { get; internal set; }
+    public string ReMix_Waklio { get; internal set; }
     private void Awake()
     {
         //URL
         WakZoo = string.Empty;
 
+        Origin_Rewind = string.Empty;
+        Origin_WinterSpring = string.Empty;
+
         BGM_01 = string.Empty;
         BGM_02 = string.Empty;
         BGM_03 = string.Empty;
 
-        Rewind = string.Empty;
-        WinterSpring = string.Empty;
+        ReMix_Rewind = string.Empty;
+        ReMix_WinterSpring = string.Empty;
 
-        NobleLick = string.Empty;
-        Wakaloid = string.Empty;
-        WakGoodAroma100 = string.Empty;
-        AvantGarde = string.Empty;
+        ReMix_NobleLick = string.Empty;
+        ReMix_Wakaloid = string.Empty;
+        ReMix_WakGoodAroma100 = string.Empty;
+        ReMix_AvantGarde = string.Empty;
 
-        YouHi = string.Empty;
-        Gotterfly = string.Empty;
-        KingADance = string.Empty;
-        IPad = string.Empty;
-        LikeDogRevival = string.Empty;
+        ReMix_YouHi = string.Empty;
+        ReMix_Gotterfly = string.Empty;
+        ReMix_KingADance = string.Empty;
+        ReMix_IPad = string.Empty;
+        ReMix_ReviveLikeADog = string.Empty;
 
-        GalUseGirl = string.Empty;
-        BangOff = string.Empty;
-        AfterTwist = string.Empty;
-        Waklio = string.Empty;
+        ReMix_GalUseGirl = string.Empty;
+        ReMix_BangOff = string.Empty;
+        ReMix_TwistedLove = string.Empty;
+        ReMix_Waklio = string.Empty;
 
         LoadConfig();
     }
@@ -65,28 +74,31 @@ public class Config : MonoBehaviourSingleton<Config>
 
         WakZoo = iniFile["URL"]["WakZoo"].ToString().Trim();
 
+        Origin_Rewind = iniFile["URL"]["Origin_Rewind"].ToString().Trim();
+        Origin_WinterSpring = iniFile["URL"]["Origin_WinterSpring"].ToString().Trim();
+
         BGM_01 = iniFile["URL"]["BGM_01"].ToString().Trim();
         BGM_02 = iniFile["URL"]["BGM_02"].ToString().Trim();
         BGM_03 = iniFile["URL"]["BGM_03"].ToString().Trim();
 
-        Rewind = iniFile["URL"]["Rewind"].ToString().Trim();
-        WinterSpring = iniFile["URL"]["WinterSpring"].ToString().Trim();
+        ReMix_Rewind = iniFile["URL"]["ReMix_Rewind"].ToString().Trim();
+        ReMix_WinterSpring = iniFile["URL"]["ReMix_WinterSpring"].ToString().Trim();
 
-        NobleLick = iniFile["URL"]["NobleLick"].ToString().Trim();
-        Wakaloid = iniFile["URL"]["Wakaloid"].ToString().Trim();
-        WakGoodAroma100 = iniFile["URL"]["WakGoodAroma100"].ToString().Trim();
-        AvantGarde = iniFile["URL"]["AvantGarde"].ToString().Trim();
+        ReMix_NobleLick = iniFile["URL"]["ReMix_NobleLick"].ToString().Trim();
+        ReMix_Wakaloid = iniFile["URL"]["ReMix_Wakaloid"].ToString().Trim();
+        ReMix_WakGoodAroma100 = iniFile["URL"]["ReMix_WakGoodAroma100"].ToString().Trim();
+        ReMix_AvantGarde = iniFile["URL"]["ReMix_AvantGarde"].ToString().Trim();
 
-        YouHi = iniFile["URL"]["YouHi"].ToString().Trim();
-        Gotterfly = iniFile["URL"]["Gotterfly"].ToString().Trim();
-        KingADance = iniFile["URL"]["KingADance"].ToString().Trim();
-        IPad = iniFile["URL"]["IPad"].ToString().Trim();
-        LikeDogRevival = iniFile["URL"]["LikeDogRevival"].ToString().Trim();
+        ReMix_YouHi = iniFile["URL"]["ReMix_YouHi"].ToString().Trim();
+        ReMix_Gotterfly = iniFile["URL"]["ReMix_Gotterfly"].ToString().Trim();
+        ReMix_KingADance = iniFile["URL"]["ReMix_KingADance"].ToString().Trim();
+        ReMix_IPad = iniFile["URL"]["ReMix_IPad"].ToString().Trim();
+        ReMix_ReviveLikeADog = iniFile["URL"]["ReMix_ReviveLikeADog"].ToString().Trim();
 
-        GalUseGirl = iniFile["URL"]["GalUseGirl"].ToString().Trim();
-        BangOff = iniFile["URL"]["BangOff"].ToString().Trim();
-        AfterTwist = iniFile["URL"]["AfterTwist"].ToString().Trim();
-        Waklio = iniFile["URL"]["Waklio"].ToString().Trim();
+        ReMix_GalUseGirl = iniFile["URL"]["ReMix_GalUseGirl"].ToString().Trim();
+        ReMix_BangOff = iniFile["URL"]["ReMix_BangOff"].ToString().Trim();
+        ReMix_TwistedLove = iniFile["URL"]["ReMix_TwistedLove"].ToString().Trim();
+        ReMix_Waklio = iniFile["URL"]["ReMix_Waklio"].ToString().Trim();
     }
 
 }
