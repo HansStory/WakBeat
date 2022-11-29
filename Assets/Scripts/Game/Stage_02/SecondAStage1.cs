6359932bc6d80692fcdb6b975166c28334201a51
KD_Han <gkswjdgur552@gmail.com>
2022-11-30 00:44:16 +0900

    Add Sample Code ( Adjust Obstacle Radius )

--- Assets/Scripts/Game/Stage_02/SecondAStage1.cs
+++ Assets/Scripts/Game/Stage_02/SecondAStage1.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 
 public class SecondAStage1 : Stage
 {
@@ -14,4 +15,16 @@ public class SecondAStage1 : Stage
     {
         base.Update();
     }
+
+    private float ballHeightHalf = 15.5f;
+    private float obstacleHeightHalf = 56.5f;
+    protected override void CreateObstacles()
+    {
+        float _inRadius = inRadius - obstacleHeightHalf + ballHeightHalf;
+        float _outRadius = outRadius + obstacleHeightHalf - ballHeightHalf;
+
+        CreateInObstacle(1, _inRadius);
+        CreateOutObstacle(1, _outRadius);
+    }
+
 }
