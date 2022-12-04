952a6ec407f0f2f1ebc9e5f6c34fba9011c14dec
KD_Han <gkswjdgur552@gmail.com>
2022-12-04 09:59:38 +0900

    Check Real Frame ( 빌드시 V-Sync를 끄지 않을경우 라인이 밀리는 현상 디버깅을 위해서 )

--- Assets/Scripts/Game/StageCharting.cs
+++ Assets/Scripts/Game/StageCharting.cs
@@ -90,6 +90,7 @@ public class StageCharting : Stage
     protected override void Update()
     {
         base.Update();
+        ShowFrame();
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -388,6 +389,60 @@ public class StageCharting : Stage
         else
         {
             TextHalfAngle.text = "Hide\nQuater Angle";
+        }
+    }
+
+    // On GUI Frame Check
+    private float deltaTime = 0.0f;
+    private bool isShow = false;
+    protected virtual void ShowFrame()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            isShow = !isShow;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Application.targetFrameRate = 30;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Application.targetFrameRate = 144;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Application.targetFrameRate = -1;
+        }
+    }
+
+    [SerializeField, Range(1, 100)] private int size = 25;
+    [SerializeField] private Color color = Color.green;
+    private void OnGUI()
+    {
+        if (isShow)
+        {
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(30, 30, Screen.width, Screen.height);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = size;
+            style.normal.textColor = color;
+
+            float ms = deltaTime * 1000f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.} FPS ({1:0.0} ms)", fps, ms);
+
+            GUI.Label(rect, text, style);
         }
     }
 }
