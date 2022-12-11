95243410307e20b6b2dd08cc6e0790dee6404c20
KD_Han <gkswjdgur552@gmail.com>
2022-12-11 21:09:40 +0900

    Fix ScriptInformation ( Add Participations - 참가자들 기록을 저장하기 위함 (영화산업(?))

--- Assets/Scripts/Data/Scriptable/ScriptInformation.cs
+++ Assets/Scripts/Data/Scriptable/ScriptInformation.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "SetupInformation", menuName = "ScriptableObjects/ScriptInformation", order = 1)]
+
+[System.Serializable]
+public class Participations
+{
+    public string Role;
+    public string[] NickName;
+}
+
 public class ScriptInformation : ScriptableObject
 {
     public string Version = "0.1.0";
 
     public string ProgramName = "Orbit or Wakbeat";
     public string Organiation = "느그게임즈";
+
+    public List<Participations> Participations;
 }
