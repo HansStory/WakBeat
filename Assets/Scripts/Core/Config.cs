de31454d3c3827e13701a168f860fb7a969abaff
KD_Han <gkswjdgur552@gmail.com>
2022-12-13 00:52:30 +0900

    Fix Config ( Add Origin Link)

--- Assets/Scripts/Core/Config.cs
+++ Assets/Scripts/Core/Config.cs
@@ -8,10 +8,21 @@ public class Config : MonoBehaviourSingleton<Config>
     public string Origin_Rewind { get; internal set; }
     public string Origin_WinterSpring { get; internal set; }
 
-    //BackGroundMusic
-    public string BGM_01 { get; internal set; }
-    public string BGM_02 { get; internal set; }
-    public string BGM_03 { get; internal set; }
+    public string Origin_NobleLick { get; internal set; }
+    public string Origin_Wakaloid { get; internal set; }
+    public string Origin_WakGoodAroma100 { get; internal set; }
+    public string Origin_AvantGarde { get; internal set; }
+
+    public string Origin_YouHi { get; internal set; }
+    public string Origin_Gotterfly { get; internal set; }
+    public string Origin_KingADance { get; internal set; }
+    public string Origin_IPad { get; internal set; }
+    public string Origin_ReviveLikeADog { get; internal set; }
+
+    public string Origin_GalUseGirl { get; internal set; }
+    public string Origin_BangOff { get; internal set; }
+    public string Origin_TwistedLove { get; internal set; }
+    public string Origin_Waklio { get; internal set; }
 
     //ReMix URL
     //Music       
@@ -33,6 +44,11 @@ public class Config : MonoBehaviourSingleton<Config>
     public string ReMix_BangOff { get; internal set; }
     public string ReMix_TwistedLove { get; internal set; }
     public string ReMix_Waklio { get; internal set; }
+
+    //BackGroundMusic
+    public string BGM_01 { get; internal set; }
+    public string BGM_02 { get; internal set; }
+    public string BGM_03 { get; internal set; }
 
     //------------------------- SYSTEM ------------------------------
     public string DEVKEY { get; internal set; }
@@ -96,16 +112,30 @@ public class Config : MonoBehaviourSingleton<Config>
 
     private void Awake()
     {
-        //URL
+        // URL
         WakZoo = string.Empty;
 
+        // Origin URL
         Origin_Rewind = string.Empty;
         Origin_WinterSpring = string.Empty;
 
-        BGM_01 = string.Empty;
-        BGM_02 = string.Empty;
-        BGM_03 = string.Empty;
+        Origin_NobleLick = string.Empty;
+        Origin_Wakaloid = string.Empty;
+        Origin_WakGoodAroma100 = string.Empty;
+        Origin_AvantGarde = string.Empty;
 
+        Origin_YouHi = string.Empty;
+        Origin_Gotterfly = string.Empty;
+        Origin_KingADance = string.Empty;
+        Origin_IPad = string.Empty;
+        Origin_ReviveLikeADog = string.Empty;
+
+        Origin_GalUseGirl = string.Empty;
+        Origin_BangOff = string.Empty;
+        Origin_TwistedLove = string.Empty;
+        Origin_Waklio = string.Empty;
+
+        // Remix URL
         ReMix_Rewind = string.Empty;
         ReMix_WinterSpring = string.Empty;
 
@@ -125,6 +155,11 @@ public class Config : MonoBehaviourSingleton<Config>
         ReMix_TwistedLove = string.Empty;
         ReMix_Waklio = string.Empty;
 
+        //BGM
+        BGM_01 = string.Empty;
+        BGM_02 = string.Empty;
+        BGM_03 = string.Empty;
+
         // SYSTEM
         DEVKEY = string.Empty;
         GAMEMODE = string.Empty;
@@ -142,13 +177,27 @@ public class Config : MonoBehaviourSingleton<Config>
 
         WakZoo = iniFile["URL"]["WakZoo"].ToString().Trim();
 
+        // Origin URL
         Origin_Rewind = iniFile["URL"]["Origin_Rewind"].ToString().Trim();
         Origin_WinterSpring = iniFile["URL"]["Origin_WinterSpring"].ToString().Trim();
 
-        BGM_01 = iniFile["URL"]["BGM_01"].ToString().Trim();
-        BGM_02 = iniFile["URL"]["BGM_02"].ToString().Trim();
-        BGM_03 = iniFile["URL"]["BGM_03"].ToString().Trim();
+        Origin_NobleLick = iniFile["URL"]["Origin_NobleLick"].ToString().Trim();
+        Origin_Wakaloid = iniFile["URL"]["Origin_Wakaloid"].ToString().Trim();
+        Origin_WakGoodAroma100 = iniFile["URL"]["Origin_WakGoodAroma100"].ToString().Trim();
+        Origin_AvantGarde = iniFile["URL"]["Origin_AvantGarde"].ToString().Trim();
 
+        Origin_YouHi = iniFile["URL"]["Origin_YouHi"].ToString().Trim();
+        Origin_Gotterfly = iniFile["URL"]["Origin_Gotterfly"].ToString().Trim();
+        Origin_KingADance = iniFile["URL"]["Origin_KingADance"].ToString().Trim();
+        Origin_IPad = iniFile["URL"]["Origin_IPad"].ToString().Trim();
+        Origin_ReviveLikeADog = iniFile["URL"]["Origin_ReviveLikeADog"].ToString().Trim();
+
+        Origin_GalUseGirl = iniFile["URL"]["Origin_GalUseGirl"].ToString().Trim();
+        Origin_BangOff = iniFile["URL"]["Origin_BangOff"].ToString().Trim();
+        Origin_TwistedLove = iniFile["URL"]["Origin_TwistedLove"].ToString().Trim();
+        Origin_Waklio = iniFile["URL"]["Origin_Waklio"].ToString().Trim();
+
+        // Remix URL
         ReMix_Rewind = iniFile["URL"]["ReMix_Rewind"].ToString().Trim();
         ReMix_WinterSpring = iniFile["URL"]["ReMix_WinterSpring"].ToString().Trim();
 
@@ -167,6 +216,11 @@ public class Config : MonoBehaviourSingleton<Config>
         ReMix_BangOff = iniFile["URL"]["ReMix_BangOff"].ToString().Trim();
         ReMix_TwistedLove = iniFile["URL"]["ReMix_TwistedLove"].ToString().Trim();
         ReMix_Waklio = iniFile["URL"]["ReMix_Waklio"].ToString().Trim();
+
+        // BGM
+        BGM_01 = iniFile["URL"]["BGM_01"].ToString().Trim();
+        BGM_02 = iniFile["URL"]["BGM_02"].ToString().Trim();
+        BGM_03 = iniFile["URL"]["BGM_03"].ToString().Trim();
 
 
         //-------------------- SYSTEM --------------------------------
