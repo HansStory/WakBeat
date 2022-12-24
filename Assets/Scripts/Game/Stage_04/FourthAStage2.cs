fbf29623688e8cd2573f8d1fb1e3f3b6cab7e766
KD_Han <gkswjdgur552@gmail.com>
2022-12-24 12:54:26 +0900

    Fix FourthAStage2

--- Assets/Scripts/Game/Stage_04/FourthAStage2.cs
+++ Assets/Scripts/Game/Stage_04/FourthAStage2.cs
@@ -2,35 +2,12 @@ using UnityEngine.UI;
 
 public class FourthAStage2 : Stage
 {
-    private GlobalData globalData;
-
     protected override void Init()
     {
-        base.Init();
-        globalData = GlobalData.Instance;
+        base.Init();        
 
-        SetBallSkin(globalData.StageInfo.BallSkins[6]);
-        SetCircleSprite(globalData.StageInfo.CircleSkins[2]);
-        SetObstacleSkin();
-    }
-
-    void SetObstacleSkin()
-    {
-        foreach (var inObjstacle in InObstacleLists)
-        {
-            var skin = inObjstacle.GetComponent<Image>();
-            if (skin = null) return;
-
-            skin.sprite = globalData.StageInfo.ObstacleSkins[3];
-        }
-
-        foreach (var outObjstacle in OutObstacleLists)
-        {
-            var skin = outObjstacle.GetComponent<Image>();
-            if (skin = null) return;
-
-            skin.sprite = globalData.StageInfo.ObstacleSkins[3];
-        }
+        SetBallSkin(GlobalData.Instance.StageInfo.BallSkins[6]);
+        SetCircleSprite(GlobalData.Instance.StageInfo.CircleSkins[2]);
     }
 
     protected override void Update()
