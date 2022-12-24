5bb0e997be9486018af59f5feb115b8d3d7b3861
KD_Han <gkswjdgur552@gmail.com>
2022-12-24 12:22:22 +0900

    Add Bang Off Resources

--- Assets/Scripts/Game/Stage_04/FourthAStage2.cs
+++ Assets/Scripts/Game/Stage_04/FourthAStage2.cs
@@ -1,10 +1,36 @@
+using UnityEngine.UI;
 
 public class FourthAStage2 : Stage
 {
+    private GlobalData globalData;
 
     protected override void Init()
     {
         base.Init();
+        globalData = GlobalData.Instance;
+
+        SetBallSkin(globalData.StageInfo.BallSkins[6]);
+        SetCircleSprite(globalData.StageInfo.CircleSkins[2]);
+        SetObstacleSkin();
+    }
+
+    void SetObstacleSkin()
+    {
+        foreach (var inObjstacle in InObstacleLists)
+        {
+            var skin = inObjstacle.GetComponent<Image>();
+            if (skin = null) return;
+
+            skin.sprite = globalData.StageInfo.ObstacleSkins[3];
+        }
+
+        foreach (var outObjstacle in OutObstacleLists)
+        {
+            var skin = outObjstacle.GetComponent<Image>();
+            if (skin = null) return;
+
+            skin.sprite = globalData.StageInfo.ObstacleSkins[3];
+        }
     }
 
     protected override void Update()
