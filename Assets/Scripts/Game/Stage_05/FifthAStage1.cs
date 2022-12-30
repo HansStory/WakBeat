0b3662a11920d9569e1e027d7447f02f41fae67f
Yesoa <114085610+Yesoaaa@users.noreply.github.com>
2022-12-30 20:57:17 +0900

    Fix Jungtur Stage Image Add

--- Assets/Scripts/Game/Stage_05/FifthAStage1.cs
+++ Assets/Scripts/Game/Stage_05/FifthAStage1.cs
@@ -1,10 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class FifthAStage1 : Stage
 {
-
     protected override void Init()
     {
         base.Init();
+
+        SetBallSkin(GlobalData.Instance.StageInfo.BallSkins[12]);
+        SetCircleSprite(GlobalData.Instance.StageInfo.CircleSkins[9]);
+        SetObstaclesSkin(9, Color.white);
+    }
+
+    protected override void PlayProcess()
+    {
+        base.PlayProcess();
+
+        switch (_currentLine)
+        {
+            case 0:
+                SetObstaclesSkin(9, Color.white);
+                break;
+            case 5:
+                SetObstaclesSkin(10, Color.white);
+                break;
+            case 10:
+                SetObstaclesSkin(11, Color.white);
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 20:
+                break;
+            case 23:
+                break;
+        }
     }
 
     protected override void Update()
