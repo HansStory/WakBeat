d9939445aa17b3c0bea00c1e6ce8ae3ceac82a25
KD_Han <gkswjdgur552@gmail.com>
2023-01-03 02:28:40 +0900

    융터르 작업 중간 Commit

--- Assets/Scripts/Game/Stage_05/FifthAStage1.cs
+++ Assets/Scripts/Game/Stage_05/FifthAStage1.cs
@@ -4,13 +4,22 @@ using UnityEngine.UI;
 
 public class FifthAStage1 : Stage
 {
+    public ParticleSystem CircleParticle;
+    private float frame;
+
     protected override void Init()
     {
         base.Init();
 
         SetBallSkin(GlobalData.Instance.StageInfo.BallSkins[12]);
+        BallSkin.color = Color.black;
+
         SetCircleSprite(GlobalData.Instance.StageInfo.CircleSkins[9]);
+        CircleSkin.color = Color.black;
+
         SetObstaclesSkin(9, Color.red);
+        frame = 1f / 60f;
+        Debug.Log(frame);
     }
 
     protected override void PlayProcess()
@@ -21,20 +30,88 @@ public class FifthAStage1 : Stage
         {
             case 0:
                 break;
-            case 5:
-                break;
-            case 10:
-                break;
             case 12:
+                Invoke(nameof(ChangeWhite), _tick * 2f);
                 break;
-            case 13:
+            case 18:
+                ChangeBlack();
                 break;
-            case 14:
+            case 50:
+                Invoke(nameof(ChangeWhite), 0.266563f);
+                Invoke(nameof(ChangeBlack), 0.433229f);
+                Invoke(nameof(ChangeWhite), 0.533229f);
+                Invoke(nameof(ChangeBlack), 0.633229f);
+                Invoke(nameof(ChangeWhite), 0.766563f);
+                Debug.Log(_playTime);
                 break;
-            case 20:
+            case 51:
+                //Invoke(nameof(ChangeWhite), 0.266563f); 
+                //Invoke(nameof(ChangeBlack), 0.433229f);
+                //Invoke(nameof(ChangeWhite), 0.533229f);
+                //Invoke(nameof(ChangeBlack), 0.633229f);
+                //Invoke(nameof(ChangeWhite), 0.766563f);
+                //Invoke(nameof(ChangeBlack), 0.433229f);
+                //Invoke(nameof(ChangeWhite), 0.533229f);
                 break;
-            case 23:
+            case 52:
+                Debug.Log(_playTime);
                 break;
+        }
+    }
+
+    void ChangeBlack()
+    {
+        ChangeBallBlack();
+        ChangeCircleBlack();
+    }
+
+    void ChangeWhite()
+    {
+        ChangeBallWhite();
+        ChangeCircleWhite();
+    }
+
+    void ChangeBallBlack()
+    {
+        BallSkin.color = Color.black;
+    }
+
+    void ChangeBallWhite()
+    {
+        BallSkin.color = Color.white;
+    }
+    void ChangeCircleBlack()
+    {
+        CircleParticle.startColor = Color.black;
+    }
+
+    void ChangeCircleWhite()
+    {
+        CircleParticle.startColor = Color.white;
+    }
+
+    public override void OnClickPause()
+    {
+        if (_isPlay)
+        {
+            _isPause = !_isPause;
+
+            if (_isPause)
+            {
+                Time.timeScale = 0;
+                audioSource.Pause();
+                CircleParticle.playbackSpeed = 0f;
+
+                if (videoPlayer) videoPlayer.Pause();
+            }
+            else
+            {
+                Time.timeScale = 1;
+                audioSource.Play();
+                CircleParticle.playbackSpeed = 1f;
+
+                if (videoPlayer) videoPlayer.Play();
+            }
         }
     }
 
